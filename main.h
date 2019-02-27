6f1b06764f06b75543232c8a933ecb105f4701ed
KlauMack <klaudijus.mackonis@gmail.com>
2019-02-27 11:07:21 +0200

    header file

---
+++ main.h
@@ -0,0 +1,22 @@
+#include <vector>
+#include <string>
+
+#ifndef HEADER_H
+#define HEADER_H
+
+struct studentas
+{
+    std::string vardas, pavarde;
+    int n = 0, egz;
+    std::vector<int> nd;
+    double vidurkis, mediana;
+};
+
+extern std::vector<studentas> stud;
+
+void print ();
+void read ();
+void galutinis (int z);
+void generate (int a);
+
+#endif
