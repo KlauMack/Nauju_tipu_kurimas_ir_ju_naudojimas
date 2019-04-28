a82c2bfb751f182018c82ed920134cb1b9191fb8
KlauMack <klaudijus.mackonis@gmail.com>
2019-04-28 18:58:17 +0300

    header file

---
+++ student.h
@@ -0,0 +1,26 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+class Student
+{
+private:
+    std::string vardas_;
+    std::string pavarde_;
+    std::vector<double> nd_;
+    double egz_;
+public:
+    Student() : egz_(0) {}
+    Student(std::string, std::string, std::vector<double>&, double);
+    std::string getVardas() const;
+    std::string getPavarde() const;
+    double getEgz() const;
+    int getSizeOfNd() const;
+    double getNd(int i) const;
+    void sortND();
+};
+
+#endif
