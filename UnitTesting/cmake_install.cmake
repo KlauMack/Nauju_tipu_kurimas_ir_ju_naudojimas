964dbdedc0329f356bdc7819438f375868d641a0
KlauMack <klaudijus.mackonis@gmail.com>
2019-05-28 20:17:32 +0300

    Google Tests

--- UnitTesting/cmake_install.cmake
+++ UnitTesting/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /mnt/c/Users/klaud/OneDrive/Stalinis kompiuteris/Uni/OP/Naujų tipų kūrimas ir jų naudojimas/UnitTesting
+# Install script for directory: ../UnitTesting
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/mnt/c/Users/klaud/OneDrive/Stalinis kompiuteris/Uni/OP/Naujų tipų kūrimas ir jų naudojimas/UnitTesting/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "../UnitTesting/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
